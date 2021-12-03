462110c02b3098fd491f12cc9eeaf67394f00c32
sarina <36801359+hackmak@users.noreply.github.com>
2021-12-03 01:34:51 -0600

    Solve number of depth increases

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,24 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun countDepthIncreases(input: List<String>): Int {
+        var prev: Int? = null
+        var counter = 0
+        for (measurement in input) {
+            val current = measurement.toIntOrNull() // assert not null
+            if (prev != null && current != null) {
+                if (prev < current) {
+                    counter += 1
+                }
+            }
+            prev = current
+        }
+        return counter
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
+    // test if implementation meets criteria from the description
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(countDepthIncreases(testInput) == 7)
 
+    // use actual input
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(countDepthIncreases(input))
 }
