afd83766859774398cb344145db06e34edef4498
sarina <36801359+hackmak@users.noreply.github.com>
2021-12-04 00:28:30 -0600

    Move Day1 challenges into the correct file

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,4 +1,5 @@
 fun main() {
+    // part 1
     fun countDepthIncreases(input: List<String>): Int {
         var prev: Int? = null
         var counter = 0
@@ -19,4 +20,31 @@ fun main() {
     // use actual input
     val input = readInput("Day01")
     println(countDepthIncreases(input))
+
+    // part 2
+    fun countSumIncreases(input: List<String>): Int {
+        var prevSum: Int? = null
+        var counter = 0
+
+        // traverse list once
+        for (i in input.indices) {
+            // only calculate sums until the last group of 3
+            if (i <= input.size - 3) {
+                val currentSum = input[i].toInt() + input[i+1].toInt() + input[i+2].toInt()
+                if (prevSum != null && prevSum < currentSum) {
+                    counter += 1
+                }
+                prevSum = currentSum
+            } else {
+                break
+            }
+        }
+        return counter
+    }
+
+    // test if implementation meets criteria from the description
+    check(countSumIncreases(testInput) == 5)
+
+    // use actual input
+    println(countSumIncreases(input))
 }
