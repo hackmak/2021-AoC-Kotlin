a739961cb811b9fd1b5f58eb653f28e424a93820
sarina <36801359+hackmak@users.noreply.github.com>
2021-12-04 00:46:52 -0600

    Solve product of depth and horizontal position with the aim calculation

--- src/Day02.kt
+++ src/Day02.kt
@@ -28,4 +28,36 @@ fun main() {
     // use actual input
     val input = readInput("Day02")
     println(calculatePosition(input))
+
+    // part 2
+    fun calculatePositionWithAim(input: List<String>): Int {
+        var horizontalPosition = 0;
+        var depth = 0
+        var aim = 0
+        for (command in input) {
+            // given commands are space separated
+            val splitCommand = command.split(" ")
+            val direction = splitCommand[0]
+            val change = splitCommand[1].toInt()
+
+            when(direction) {
+                "down" -> aim += change
+                "up" -> aim -= change
+                "forward" -> {
+                    horizontalPosition += change
+                    depth += aim * change
+                }
+                else -> {
+                    print("invalid input given")
+                }
+            }
+        }
+        return horizontalPosition * depth
+    }
+
+    // test if implementation meets criteria from the description
+    check(calculatePositionWithAim(testInput) == 900)
+
+    // use actual input
+    println(calculatePositionWithAim(input))
 }
