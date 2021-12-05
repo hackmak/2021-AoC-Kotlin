94799fd35b91b258f55d2dd0bba2d3cd04d7fd60
sarina <36801359+hackmak@users.noreply.github.com>
2021-12-04 19:53:44 -0600

    Refactor Day01 to use Int input. Part 2 now uses the same method from Part 1 for better readability.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,29 +1,31 @@
 fun main() {
     // part 1
-    fun countDepthIncreases(input: List<String>): Int {
+    fun countDepthIncreases(input: List<Int>): Int {
         return input.filterIndexed { i, _ ->
             if (i != 0)
-                input[i].toInt() > input[i - 1].toInt()
+                // check if current value is greater than previous
+                input[i] > input[i - 1]
             else false
         }.size
     }
 
     // test if implementation meets criteria from the description
-    val testInput = readInput("Day01_test")
+    val testInput = readInputAsInt("Day01_test")
     check(countDepthIncreases(testInput) == 7)
 
     // use actual input
-    val input = readInput("Day01")
+    val input = readInputAsInt("Day01")
     println(countDepthIncreases(input))
 
     // part 2
-    fun countSumIncreases(input: List<String>): Int {
-        return input.filterIndexed { i, _ ->
-            if (i != 0 && i + 2 < input.size)
-                // start with second group of 3
-                input[i].toInt() + input[i + 1].toInt() + input[i + 2].toInt() > input[i - 1].toInt() + input[i].toInt() + input[i + 1].toInt()
-            else false
-        }.size
+    fun countSumIncreases(input: List<Int>): Int {
+        // create new list of sums
+        val sumList = ArrayList<Int>()
+        for (i in 0..input.size - 3) {
+            // check if sum of next 3 values starting with current index is greater than the sum from the previous index
+            sumList.add(i, (input[i] + input[i + 1] + input[i + 2]))
+        }
+        return countDepthIncreases(sumList)
     }
     // test if implementation meets criteria from the description
     check(countSumIncreases(testInput) == 5)
