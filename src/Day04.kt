9bdc250171ef439b6ed2e251209176f485b66709
sarina <36801359+hackmak@users.noreply.github.com>
2021-12-05 23:47:51 -0600

    Get accumulated boards with marked bingo numbers using fold

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,49 +1,83 @@
 fun main() {
     // part 1
-    class BingoObject constructor(number: Int, isMarked: Boolean) {
-        var number = number
-            get() = field
-            set(value) {
-                field = value
-            }
+    data class BingoObject (val number: Int, val isMarked: Boolean)
 
-        var isMarked = isMarked
-            get() = field
-            set(value) {
-                field = value
+    fun markBingoBoard(bingoNumber: Int, currentBoard: List<List<BingoObject>>): List<List<BingoObject>> {
+        return currentBoard.map { bingoList ->
+            bingoList.map { bingoObject ->
+                if (bingoObject.number == bingoNumber)
+                    BingoObject(bingoObject.number, true)
+                else bingoObject
             }
+        }
+    }
+
+    fun isWinningBoard(currentBoard: List<List<BingoObject>>) {
+        // todo will need to calculate score
+        currentBoard.forEach { bingoList ->
+            bingoList.map {
+                // check horizontal wins first
+
+            }
+        }
     }
 
     fun calculateBingoScore(input: List<String>): Int {
         // number of draws is first line of input
         val numberDraws = input[0].split(",").map { draw -> draw.toInt() }
-        val allBoards = ArrayList<List<List<BingoObject>>>()
 
         // filter input to get rid of starting whitespace, number draws, and new lines
         var filteredInput = input.map { line -> line.trim() }
             .filterIndexed { i, line ->
                 i != 0 && line != "" // ignore first line which is number draws and any new lines
             }
+        val allBoards = ArrayList<List<List<BingoObject>>>()
 
         // get all bingo tables and add to a list
         while(filteredInput.size >= 5) {
             // get first 5 elements
-            val bingoBoard = filteredInput.take(5).map {
-                line -> line.split("\\s+".toRegex()).map {
+            allBoards.add(filteredInput.take(5).map {
+                line -> line.split("\\s+".toRegex()).map { // split by whitespace
                     bingoNumber -> BingoObject(bingoNumber.toInt(), false)
-                } //.toTypedArray()
-            } //.toTypedArray()
-
-            println(bingoBoard)
-            allBoards.add(bingoBoard)
-
+                }
+            })
             // remove first 5 elements
             filteredInput = filteredInput.drop(5)
         }
 
-        // go through each number drawn and mark each bingo board if they have the value
+        val boardListAll = allBoards.toList() // fixme will need to set allBoards functionally
+//        val test = numberDraws.mapIndexed { i, number ->
+//            println(number)
+//            boardListAll.fold(boardListAll) { boardList, board ->
+//                return@fold markBingoBoards(number, boardList)
+//            }
+//
+//        }
 
-        // check if each board wins after 5+ moves
+        // go through each number drawn and mark each bingo board if they have the value
+        val test1 = numberDraws.fold(boardListAll) { boardList, number ->
+                boardList.map { board ->
+                    markBingoBoard(number, board)
+                    // check if each board wins todo after 5+ moves
+
+                }
+        }
+        println(test1)
+//        for (i in numberDraws.indices) {
+//            allBoards.map { board ->
+//                board.fold(board) { _, _ ->
+//                    markBingoBoard(numberDraws[i], board)
+//                }
+//            }
+
+            //allBoards.fold(allBoards.map { board -> board.fold(board) {markBingoBoard(numberDraws[i], board) } })
+
+
+//            if (i >= 4) {
+//                // check if each board wins after 5+ moves
+//
+//            }
+
         return 0
     }
 
